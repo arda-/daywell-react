5c48964d583ff66a6575f0ebee38afd301c4acd5
Arda Ungun <arda@ungun.me>
2023-03-11 00:10:22 +0300

    get dropdown kinda working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
 import {
   TrashIcon,
+  CheckIcon, 
+  ChevronDownIcon,
 } from '@heroicons/react/20/solid'
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react'
+
+
 
 
 const ToggleButton = (props) => {
@@ -30,6 +35,105 @@ const ToggleButton = (props) => {
   )
 }
 
+
+
+
+
+const people = [
+  { id: 1, name: 'Leslie Alexander' },
+  { id: 2, name: 'banana Alexander' },
+  { id: 3, name: 'cedric Alexander' },
+  { id: 4, name: 'delta Alexander' },
+]
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export function Dropdown() {
+
+  const tags = [
+    {
+      active: false,
+      link: "#",
+      text: "none"
+    },
+    {
+      active: true,
+      link: "#",
+      text: "English"
+    },
+    {
+      active: false,
+      link: "#",
+      text: "Math"
+    }
+  ]
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="
+          inline-flex w-full justify-center 
+          gap-x-1.5 rounded-md bg-white 
+          px-2 py-1 
+          text-sm text-gray-800 
+          ring-1 ring-inset ring-gray-300 
+          hover:ring-sky-600
+        "
+          
+        >
+          {tags.filter(t => t.active)[0].text}
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items 
+          className="absolute left-0 z-10 mt-2 w-48 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            {tags.map(tag => 
+              <Menu.Item key={tag.text}>
+                  <button
+                    href={tag.link}
+                    className={classNames(
+                      tag.active ? 'bg-sky-100 text-gray-900' : 'text-gray-700',
+                      'px-4 py-2 text-sm',
+                      'hover:bg-sky-200',
+                      'w-full text-left flex justify-between'
+                    )}
+                  >
+                    <span>
+                      {tag.text}
+                    </span>
+                    {tag.active && (
+                      <span
+                        className={classNames(
+                          'inset-y top-0 right-0 flex items-center',
+                          tag.active ? 'text-sky-900' : 'text-indigo-600'
+                        )}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </button>
+              </Menu.Item>
+            )}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
 
 
 
@@ -84,16 +188,37 @@ const Task = (props) => {
 
   const taskClasses = `
     px-3 py-1.5 my-2 hover:bg-sky-100
-    ${editing && "shadow bg-sky-100"}
+    ${editing && "shadow-md bg-sky-100"}
   `;
 
   const centerAreaClasses = `
-    ml-1 py-1 px-2
-    leading-6 w-full 
-    hover:bg-sky-200 active:bg-sky-300 
-    rounded-md 
-    cursor-pointer
+    
   `;
+
+  const uneditableCenterArea = (
+    <>
+      <label htmlFor="comments" className="font-medium text-gray-900">
+        {props.text}
+      </label>
+      <p id="comments-description" className="text-gray-500 text-sm">
+        {props.tag}
+      </p>
+    </>
+  );
+
+  const editableCenterArea = (
+    <>
+      <label htmlFor="comments" className="font-medium text-gray-900">
+        {props.text}
+      </label>
+      <div>
+        <span className='text-gray-70 text-sm mr-1'>TAG:</span>
+        <Dropdown/>
+      </div>
+    </>
+  );
+
+  
 
   return (
     <div className={taskClasses}>
@@ -103,15 +228,16 @@ const Task = (props) => {
           {editing && trash}
         </div>
         <div 
-          className={centerAreaClasses}
+          className='
+            ml-1 py-1 px-2
+            leading-6 w-full 
+            hover:bg-sky-200 active:bg-sky-300 
+            rounded-md 
+            cursor-pointer
+            flex flex-col items-start'
           onClick={toggleEditing}  
         >
-          <label htmlFor="comments" className="font-medium text-gray-900">
-            {props.text}
-          </label>
-          <p id="comments-description" className="text-gray-500 text-sm">
-            {props.tag}
-          </p>
+          {editing ? editableCenterArea : uneditableCenterArea}
         </div>
         <div className="flex">
           <ToggleButton 
@@ -194,6 +320,7 @@ export default function Home() {
 
   return (
     <div>
+      <Dropdown />
       {tempTasks.map((t) => <Task key={t.id} {...t}/>)}
     </div>
     
