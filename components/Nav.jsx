19bdee95c1ff2932185dc615800646e4a2fdd4e2
Arda Ungun <arda@ungun.me>
2023-11-14 01:04:03 -0500

    text color finesse - make grays neutral

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,7 +20,7 @@ export default function Example() {
             <div className="relative flex h-14 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-amber-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-amber-700">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-neutral-400 hover:bg-amber-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-amber-700">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -47,7 +47,7 @@ export default function Example() {
                         className={classNames(
                           item.current
                             ? "bg-amber-900 text-white"
-                            : "text-gray-300 hover:bg-amber-800 hover:text-white",
+                            : "text-neutral-300 hover:bg-amber-800 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}
@@ -61,7 +61,7 @@ export default function Example() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="relative rounded-full bg-amber-950 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-amber-700 focus:ring-offset-2 focus:ring-offset-amber-700"
+                  className="relative rounded-full bg-amber-950 p-1 text-neutral-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-amber-700 focus:ring-offset-2 focus:ring-offset-amber-700"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
@@ -97,7 +97,7 @@ export default function Example() {
                             href="#"
                             className={classNames(
                               active ? "bg-amber-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-sm text-neutral-700"
                             )}
                           >
                             Your Profile
@@ -110,7 +110,7 @@ export default function Example() {
                             href="#"
                             className={classNames(
                               active ? "bg-amber-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-sm text-neutral-700"
                             )}
                           >
                             Settings
@@ -123,7 +123,7 @@ export default function Example() {
                             href="#"
                             className={classNames(
                               active ? "bg-amber-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-sm text-neutral-700"
                             )}
                           >
                             Sign out
@@ -160,7 +160,7 @@ export default function Example() {
                   className={classNames(
                     item.current
                       ? "bg-amber-900 text-white"
-                      : "text-gray-300 hover:bg-amber-800 hover:text-white",
+                      : "text-neutral-300 hover:bg-amber-800 hover:text-white",
                     "block rounded-md px-3 py-2 text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
