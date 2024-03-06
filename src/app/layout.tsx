ca71c934e7aa07ea96c5ab1b6eeaa1c35351afc5
Arda Ungun <arda@ungun.me>
2024-03-05 22:06:16 -0500

    remove client architecture, nav succesfully loads cursor data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ClerkProvider } from "@clerk/nextjs";
 
 // import type { Metadata } from "next";
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  UserButton,
+  SignInButton,
+  currentUser,
+} from "@clerk/nextjs";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
@@ -7,11 +13,43 @@ const navigation = [
   { name: "Calendar", href: "#", current: false },
 ];
 
-export default function Example() {
+async function AccountCorner() {
+  const user = await currentUser();
+  let userZone = null;
+
+  if (!user) {
+    userZone = (
+      <div>
+        <ClerkLoading>
+          <div>Sign-in button loading...</div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignInButton />
+        </ClerkLoaded>
+      </div>
+    );
+  } else {
+    userZone = (
+      <div>
+        <ClerkLoading>
+          <div>UserButton loading...</div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
+      </div>
+    );
+  }
+
+  return userZone;
+}
+
+export default function Nav() {
   return (
     <div className="flex justify-between items-center bg-amber-600 p-2 sticky top-0 z-50">
       <div>Nav bar will eventually go here.</div>
-      <UserButton />
+
+      <AccountCorner />
     </div>
   );
 }
